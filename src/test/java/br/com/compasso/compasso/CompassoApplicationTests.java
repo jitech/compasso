0876ca863c2e6ac55e8e36762c8dc5376d9afcd4
Jonas <jgm.melo@gmail.com>
2020-01-31 13:15:05 -0300

    Create project

---
+++ src/test/java/br/com/compasso/compasso/CompassoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compasso.compasso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
